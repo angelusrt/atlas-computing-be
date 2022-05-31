4de8d9f5099f23ec846821ab340780dbdd2a2359
Angelus Trindade <angelusrt@gmail.com>
2022-05-31 00:51:58 -0300

    📩 Get todos os posts adicionado

--- routes/post.js
+++ routes/post.js
@@ -1,6 +1,19 @@
 const router = require("express").Router()
 const Post = require("../models/Post.js")
 
+//Gets all Post
+router.get("/", async(req, res) => {
+  try{
+    //Gets post
+    const post = await Post.find({}, "_id tags title")
+
+    //Sends its
+    res.json(post)
+  } catch(err){
+    res.status(400).json(err)
+  }
+})
+
 //Gets Post
 router.get("/:postID", async(req, res) => {
   try{
