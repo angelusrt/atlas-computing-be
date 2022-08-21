4e3ef47a4954936bc22c3a81c80ac319b6555bcc
Angelus Rocha Trindade Garcia <angelusrt@gmail.com>
2022-08-21 01:57:30 +0000

    👨‍🔧 Correçõ adicionadas

--- routes/post.js
+++ routes/post.js
@@ -1,6 +1,15 @@
 const router = require("express").Router()
 const Post = require("../models/Post.js")
 
+//Test
+router.get("/", async(req, res) => {
+   try{
+     res.status(200).send("Yooo")
+   } catch(err){
+     res.status(400).json(err)
+   }
+})
+
 //Gets all Post
 router.get("/:lang", async(req, res) => {
   try{
@@ -34,11 +43,29 @@ router.get("/:lang", async(req, res) => {
 })
 
 //Gets Post
+<<<<<<< Updated upstream
+=======
+//router.get("/:postID", async(req, res) => {
+//  try{
+//    //Gets post
+//    const post = await Post.findById(req.params.postID)
+//
+//    //Sends its
+//    res.json(post)
+//  } catch(err){
+//    res.status(400).json(err)
+//  }
+//})
+
+//Gets Post
+>>>>>>> Stashed changes
 router.get("/:lang/:postID", async(req, res) => {
   try{
+    console.log(req)
     //Gets post
     const post = await Post.findById(req.params.postID)
 
+    console.log("post ", post)
     //Treats information
     const newPost = {
       date: post.date,
